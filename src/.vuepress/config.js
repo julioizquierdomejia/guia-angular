e271fc093eaf2b3cdc76773767fb6c41c4f48007
Julio Izquierdo Mejia <julio.izquierdo.mejia@gmail.com>
2023-01-04 16:25:35 -0500

    commit docs build

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,7 +3,7 @@ const { description } = require('../../package')
 module.exports = {
 
     dest: 'docs',
-    base: 'https://julioizquierdomejia.github.io/guia-angular/',
+    base: '/guia-angular/',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
